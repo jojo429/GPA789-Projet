a5a8f359201bb425ed842429c28ce8a7aac83f46
LAPTOP-TEP8E8FQ\June <genevieve.dao.phan@gmail.com>
2017-11-28 18:41:01 -0500

    QGraphicsScene, QGraphicsView, QGraphicsItem added.

--- GPA789-Projet/GPA789Projet.cpp
+++ GPA789-Projet/GPA789Projet.cpp
@@ -1,7 +1,53 @@
 #include "GPA789Projet.h"
 
+#include <QGraphicsScene>
+#include <QGraphicsGridLayout>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QGraphicsWidget>
+#include <QGraphicsProxyWidget>
+#include <QWidget>
+#include <QScrollBar>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSlider>
+
 GPA789Projet::GPA789Projet(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	QGraphicsScene *animationScene = new QGraphicsScene(0, 0, 5000, 800);
+	QGraphicsScene *settingsScene = new QGraphicsScene(0, 0, 250, 800);
+
+	QHBoxLayout *firstLvlSettings = new QHBoxLayout;
+
+	QSlider *luminositySlider = new QSlider;
+	QSlider *rainSlider = new QSlider;
+
+	firstLvlSettings->addWidget(luminositySlider);
+	firstLvlSettings->addWidget(rainSlider);
+
+	QVBoxLayout *settingsLayout = new QVBoxLayout;
+
+	settingsLayout->addLayout(firstLvlSettings);
+
+	QWidget *settingsWidget = new QWidget;
+	settingsWidget->setLayout(settingsLayout);
+
+	settingsScene->addWidget(settingsWidget);
+
+	QGraphicsView *animationView = new QGraphicsView(animationScene, Q_NULLPTR);
+	QGraphicsView *settingsView = new QGraphicsView(settingsScene, Q_NULLPTR);
+
+	QHBoxLayout *layout = new QHBoxLayout;
+	layout->addWidget(animationView);
+	layout->addWidget(settingsView);
+
+	QWidget *mainWindow = new QWidget;
+	mainWindow->setLayout(layout);
+
+	setCentralWidget(mainWindow);
+
 }
