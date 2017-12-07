59046a2829f8fa4264ac4474d8be6c5303bb5187
Joé Charest <joe.charest.1@etsmtl.net>
2017-12-07 11:51:16 -0500

    Correction du boundingrect de la seed

--- GPA789-Projet/QSeeds.cpp
+++ GPA789-Projet/QSeeds.cpp
@@ -62,7 +62,8 @@ void QSeeds::move()
 QRectF QSeeds::boundingRect() const
 {
 	//QRectF(qreal x, qreal y, qreal width, qreal height)
-	return QRectF(0.0 - mSeedRadius, 0.0 - mSeedRadius, mSeedRadius * 2.0, mSeedRadius * 2.0);
+	//return QRectF(0.0 - mSeedRadius, 0.0 - mSeedRadius, mSeedRadius * 2.0, mSeedRadius * 2.0);
+	return QRectF(0.0 - mSeedRadius, 0.0 - (mSeedRadius - mSeedRadius * 0.5), mSeedRadius, mSeedRadius - mSeedRadius * 0.5);
 }
 
 void QSeeds::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
