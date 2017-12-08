581dd0ee93318532fcc30985175a8aaaba6d7eb2
LAPTOP-TEP8E8FQ\June <genevieve.dao.phan@gmail.com>
2017-12-07 21:53:14 -0500

    Génération de la map de fertilité (reste à faire une optimisation du code).

--- GPA789-Projet/RandomUniform.cpp
+++ GPA789-Projet/RandomUniform.cpp
@@ -15,3 +15,8 @@ RandomUniform::~RandomUniform()
 int RandomUniform::random() {
 	return  mDistribution(RandomGenerator::mMT32);
 }
+
+int RandomUniform::random(int min, int max)
+{
+	return std::uniform_int_distribution<int>(min, max)(mMT32);
+}
